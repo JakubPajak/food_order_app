82b7e90f9a3e24a13318db492a2a9e83ff193da7
Jakub Pająk <jakubpajak@MacBook-Pro-Jakub-2.local>
2024-03-02 20:02:54 +0100

    Reset password via email added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:food_order_app/src/presentation/pages/forgot_password_page.dart';
 import 'package:food_order_app/src/presentation/pages/home_page.dart';
 import 'package:food_order_app/src/presentation/pages/login_page.dart';
 import 'package:food_order_app/src/presentation/pages/profile_oage.dart';
@@ -27,6 +28,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/login': (context) => const LoginPage(),
         '/signup': (context) => const SignUpPage(),
+        '/forgotpass': (context) => const ForgotPasswordPage(),
         '/home': (context) => const HomePage(),
         '/search': (context) => const SearchPage(),
         '/profile': (context) => const ProfilePage(),
